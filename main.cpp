09a9a37194f3dd55959778563dca578db9a4633b
Artem Lantsman <artemlantsman05@gmail.com>
2023-04-11 14:57:20 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -9,6 +9,12 @@ using namespace std;
 using namespace hazedumper::netvars;
 using namespace hazedumper::signatures;
 
+struct color{
+    color(float r, float g, float b, float a = 1.f):
+        r(r), g(g), b(b), a(a) {}; 
+    float r, g, b, a;
+} t = color(0.87, 0.6, 0.2, 0.8), ct = color(0.36, 0.47, 0.68, 0.8);
+
 HANDLE hProc;
 DWORD procId;
 uintptr_t clientMod;
@@ -27,7 +33,7 @@ datatype rpm(DWORD address){
 void bhop(){
     int locPlayer, locFlags;
     while(true){
-        if(rpm<int>(clientMod + dwEntityList) != 0){
+        if(rpm<int>(clientMod + dwEntityList)){
             locPlayer = rpm<int>(clientMod + dwLocalPlayer);
             locFlags = rpm<int>(locPlayer + m_fFlags);
             if(locFlags == 257 && GetAsyncKeyState(VK_SPACE) || locFlags == 263 && GetAsyncKeyState(VK_SPACE)){
@@ -42,7 +48,7 @@ void bhop(){
 void radar(){
     int entity;
 	while(true){
-        if(rpm<int>(clientMod + dwEntityList) != 0){
+        if(rpm<int>(clientMod + dwEntityList)){
             for(short int i = 1; i <= 64; i++){
                 entity = rpm<int>(clientMod + dwEntityList + i * 0x10);
                 if(entity + m_bDormant != 1)
@@ -56,7 +62,7 @@ void radar(){
 void glow(){
     int locPlayer, glowObjectManager, entity, glowIndex;
     while(true){
-        if(rpm<int>(clientMod + dwEntityList) != 0){
+        if(rpm<int>(clientMod + dwEntityList)){
             locPlayer = rpm<int>(clientMod + dwLocalPlayer);
             glowObjectManager = rpm<int>(clientMod + dwGlowObjectManager);
             for(int i = 1; i <= 64; i++){
@@ -64,16 +70,10 @@ void glow(){
                 if(entity + m_bDormant != 1){
                     glowIndex = rpm<int>(entity + m_iGlowIndex);
                     if(rpm<int>(entity + m_iTeamNum) == 2){
-                        wpm<float>(glowObjectManager + glowIndex * 0x38 + 0x8, 0.87f);
-                        wpm<float>(glowObjectManager + glowIndex * 0x38 + 0xC, 0.6f);
-                        wpm<float>(glowObjectManager + glowIndex * 0x38 + 0x10, 0.2f);
-                        wpm<float>(glowObjectManager + glowIndex * 0x38 + 0x14, 0.8f);
+                        wpm<color>(glowObjectManager + glowIndex * 0x38 + 0x8, t);
                     }
                     else{
-                        wpm<float>(glowObjectManager + glowIndex * 0x38 + 0x8, 0.36f);
-                        wpm<float>(glowObjectManager + glowIndex * 0x38 + 0xC, 0.47f);
-                        wpm<float>(glowObjectManager + glowIndex * 0x38 + 0x10, 0.68f);
-                        wpm<float>(glowObjectManager + glowIndex * 0x38 + 0x14, 0.8f);
+                        wpm<color>(glowObjectManager + glowIndex * 0x38 + 0x8, ct);
                     }
                     wpm<bool>(glowObjectManager + glowIndex * 0x38 + 0x27, true);
                     wpm<bool>(glowObjectManager + glowIndex * 0x38 + 0x28, true);
@@ -84,9 +84,27 @@ void glow(){
     }
 }
 
+void triggerbot(){
+    int locPlayer, crosshairId, player;
+    while(true){
+        if(rpm<int>(clientMod + dwEntityList)){
+            locPlayer = rpm<int>(clientMod + dwLocalPlayer);
+            if(!rpm<int>(locPlayer + m_iHealth)) continue;
+            crosshairId = rpm<int>(locPlayer + m_iCrosshairId);
+            if(!crosshairId || crosshairId > 64) continue;
+            player = rpm<int>(clientMod + dwEntityList + (crosshairId - 1) * 0x10);
+            if(!rpm<int>(player + m_iHealth)) continue;
+            if(rpm<int>(locPlayer + m_iTeamNum) == rpm<int>(player + m_iTeamNum)) continue;
+            wpm<int>(clientMod + dwForceAttack, 6);
+            Sleep(30);
+            wpm<int>(clientMod + dwForceAttack, 4);
+        } else Sleep(200);
+    }
+}
+
 int main(){
-    string names[] = {"Bhop", "Radar hack", "Glow ESP"};
-    bool arr[] = {false, false, false};
+    string names[] = {"Bhop", "Radar hack", "Glow ESP", "Triggerbot"};
+    bool arr[] = {false, false, false, false};
     int choice = 1, max = 0;
     char input;
     for(int i = 0; i < sizeof(names) / sizeof(names[0]); i++)
@@ -117,7 +135,7 @@ int main(){
 	getProcess(hProc, procId, "csgo.exe");
 	clientMod = getModule(procId, "client.dll");
 	cout << hProc << " " << procId << " " << "0x" << hex << clientMod << dec << endl;
-    void (*functions[3])() = {bhop, radar, glow};
+    void (*functions[4])() = {bhop, radar, glow, triggerbot};
     queue<thread> threads;
     for(int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
         if(arr[i] == true)
